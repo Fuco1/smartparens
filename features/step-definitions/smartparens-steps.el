8051bad2d3aac3046e0a959d819a081f2997a1e1
Takafumi Arakaki <aka.tkf@gmail.com>
2013-02-18 16:27:30 +0100

    Run: ecukes --new

---
+++ features/step-definitions/smartparens-steps.el
@@ -0,0 +1,28 @@
+;; This file contains your project specific step definitions. All
+;; files in this directory whose names end with "-steps.el" will be
+;; loaded automatically by Ecukes.
+
+(Given "^I have \"\\(.+\\)\"$"
+       (lambda (something)
+         ;; Do something
+         ))
+
+(When "^I have \"\\(.+\\)\"$"
+      (lambda (something)
+        ;; Do something
+        ))
+
+(Then "^I should have \"\\(.+\\)\"$"
+      (lambda (something)
+        ;; Do something
+        ))
+
+(And "^I have \"\\(.+\\)\"$"
+     (lambda (something)
+       ;; Do something
+       ))
+
+(But "^I should not have \"\\(.+\\)\"$"
+     (lambda (something)
+       ;; Do something
+       ))
---
+++ features/support/env.el
@@ -0,0 +1,37 @@
+;; This is an example of how you could set up this file. This setup
+;; requires a directory called util in the project root and that the
+;; util directory contains the testing tools ert and espuds.
+
+(let* ((features-directory
+        (file-name-directory
+         (directory-file-name (file-name-directory load-file-name))))
+       (project-directory
+        (file-name-directory
+         (directory-file-name features-directory))))
+  (setq smartparens-root-path project-directory)
+  (setq smartparens-util-path (expand-file-name "util" smartparens-root-path)))
+
+(add-to-list 'load-path smartparens-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" smartparens-util-path))
+(add-to-list 'load-path (expand-file-name "ert" smartparens-util-path))
+
+(require 'smartparens)
+(require 'espuds)
+(require 'ert)
+
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
