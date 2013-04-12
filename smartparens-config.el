591c905f26f8a5bf7c81ed83dfaee1f7d6c63a77
Matus Goljer <dota.keys@gmail.com>
2013-04-12 02:34:41 +0200

    Update version & Legal stuff

--- smartparens-config.el
+++ smartparens-config.el
@@ -1,6 +1,6 @@
 ;;; smartparens-config.el --- Default configuration for smartparens package
 
-;; Copyright (C) 2012 Matus Goljer
+;; Copyright (C) 2013 Matus Goljer
 
 ;; Author: Matus Goljer <matus.goljer@gmail.com>
 ;; Maintainer: Matus Goljer <matus.goljer@gmail.com>
@@ -12,18 +12,20 @@
 
 ;;; License:
 
-;; This program is free software; you can redistribute it and/or modify
+;; This file is part of Smartparens.
+
+;; Smartparens is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; Smartparens is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with Smartparens.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- smartparens-pkg.el
+++ smartparens-pkg.el
@@ -1,5 +1,5 @@
 (define-package
   "smartparens"
-  "1.4"
+  "1.4.3"
   "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
   '((dash "1.1.0")))
