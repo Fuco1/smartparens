508790310e8a1566470634747afdc997b4a4fff9
Matus Goljer <dota.keys@gmail.com>
2013-11-06 12:32:20 +0100

    Update the header information

--- smartparens.el
+++ smartparens.el
@@ -29,7 +29,23 @@
 
 ;;; Commentary:
 
-;; See github readme at https://github.com/Fuco1/smartparens
+;; Smartparens is minor mode for Emacs that deals with parens pairs
+;; and tries to be smart about it. It started as a unification effort
+;; to combine functionality of several existing packages in a single,
+;; compatible and extensible way to deal with parentheses, delimiters,
+;; tags and the like. Some of these packages include autopair,
+;; textmate, wrap-region, electric-pair-mode, paredit and others. With
+;; the basic features found in other packages it also brings many
+;; improvements as well as completely new features.
+
+;; For a basic overview, see github readme at
+;; https://github.com/Fuco1/smartparens
+
+;; For the complete documentation visit the documentation wiki located
+;; at https://github.com/Fuco1/smartparens/wiki
+
+;; If you like this project, you can donate here:
+;; https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CEYP5YVHDRX8C
 
 ;;; Code:
 
