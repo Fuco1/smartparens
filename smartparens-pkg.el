d3c02bbf4ebc95da51b7049e8db7458781f3c190
Matus Goljer <dota.keys@gmail.com>
2013-12-12 14:49:16 +0100

    Bump dependencies

--- smartparens-pkg.el
+++ smartparens-pkg.el
@@ -2,4 +2,4 @@
   "smartparens"
   "1.6.1"
   "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
-  '((cl-lib "0.1") (dash "2.3.0")))
+  '((cl-lib "0.3") (dash "2.4.0")))
