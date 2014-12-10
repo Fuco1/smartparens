fc02a1bd4a4942d47730af3a1b701ee3223aae83
Markus Hauck <markus1189@gmail.com>
2014-12-10 09:54:14 +0100

    Replace delete-backward-char with delete-char
    delete-backward-char is for interactive use

--- smartparens.el
+++ smartparens.el
@@ -6129,7 +6129,7 @@ Examples:
           (setq items (nreverse items))
           (save-excursion
             (goto-char end)
-            (delete-backward-char (length cl))
+            (delete-char (- (length cl)))
             (while items
               (sp-get (car items)
                 (goto-char :end)
