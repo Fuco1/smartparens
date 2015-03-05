98aa20daf907a1a4adfc587f8d7f878e40f13d6e
Russ Tyndall <russ@acceleration.net>
2015-03-05 15:59:34 -0500

    Allow shift selection with forward/backward sexps
    re Fuco1/smartparens#360

--- smartparens.el
+++ smartparens.el
@@ -4452,7 +4452,7 @@ Examples: (prefix arg in comment)
   (|foo bar baz)   -> (foo bar| baz) ;; 2
 
   (foo (bar baz|)) -> (foo (bar baz)|)"
-  (interactive "p")
+  (interactive "^p")
   (setq arg (or arg 1))
   (if (< arg 0)
       (sp-backward-sexp (- arg))
@@ -4463,6 +4463,8 @@ Examples: (prefix arg in comment)
         (setq n (1- n))
         (when ok (goto-char (sp-get ok :end))))
       ok)))
+
+(put 'sp-forward-sexp  'CUA 'move)
 
 (defun sp-backward-sexp (&optional arg)
   "Move backward across one balanced expression (sexp).
@@ -4484,7 +4486,7 @@ Examples: (prefix arg in comment)
   (foo bar| baz)   -> (|foo bar baz) ;; 2
 
   (|(foo bar) baz) -> ((|foo bar) baz)"
-  (interactive "p")
+  (interactive "^p")
   (setq arg (or arg 1))
   (if (< arg 0)
       (sp-forward-sexp (- arg))
@@ -4495,6 +4497,8 @@ Examples: (prefix arg in comment)
         (setq n (1- n))
         (when ok (goto-char (sp-get ok :beg))))
       ok)))
+
+(put 'sp-backward-sexp 'CUA 'move)
 
 (defun sp-next-sexp (&optional arg)
   "Move forward to the beginning of next balanced expression.
