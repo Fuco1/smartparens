d6c199a152873083be6352795efcde618a07b60b
Wilfred Hughes <me@wilfred.me.uk>
2015-11-03 23:06:32 +0000

    Handle ' in rust-mode intelligently.
    Fixes #512.

---
+++ test/smartparens-rust-test.el
@@ -0,0 +1,24 @@
+(require 'rust-mode)
+(require 'smartparens)
+
+(ert-deftest sp-test-rust-parameterized-lifetimes ()
+  "When inserting ' in a <>, don't treat it as a character."
+  (sp-test-with-temp-buffer "fn foo<|>(x: T) {}"
+      (rust-mode)
+    (execute-kbd-macro "'a")
+    (should (equal (buffer-string) "fn foo<'a>(x: T) {}"))))
+
+(ert-deftest sp-test-rust-ampersand-parameter ()
+  "When inserting ' in a &'foo, don't treat it as a character."
+  (sp-test-with-temp-buffer "fn foo(x: |T) {}"
+      (rust-mode)
+    (execute-kbd-macro "&'a ")
+    (should (equal (buffer-string) "fn foo(x: &'a T) {}"))))
+
+(ert-deftest sp-test-rust-character-literal ()
+  "When inserting ' for a character literal, insert the closing '."
+  (sp-test-with-temp-buffer "let x = |"
+      (rust-mode)
+    (execute-kbd-macro "'a")
+    (should (equal (buffer-string) "let x = 'a'"))))
+
