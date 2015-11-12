b39610fb51f02e39fc9942db6c14ec79b148354a
Wilfred Hughes <me@wilfred.me.uk>
2015-11-12 22:32:58 +0000

    Don't reindent in python-mode.
    Fixes #529.

---
+++ test/smartparens-python-test.el
@@ -0,0 +1,19 @@
+(ert-deftest sp-test-sp-autoescape-string-quote-if-empty ()
+  (sp-test-with-temp-buffer "def foo():\n    |"
+      (python-mode)
+    (let ((sp-autoescape-string-quote-if-empty '(python-mode))
+          (sp-autoescape-string-quote t)
+          (sp-autoskip-closing-pair 'always)
+          (sp-undo-pairs-separately nil))
+      (execute-kbd-macro "\"\"\""))
+    (should (equal (buffer-string) "def foo():\n    \"\"\"\"\"\""))))
+
+(ert-deftest sp-test-dont-reindent-python ()
+  (sp-test-with-temp-buffer "if foo:
+    bar()
+baz = biz.boz|"
+      (python-mode)
+    (sp-backward-kill-word 1)
+    (should (equal (buffer-string) "if foo:
+    bar()
+baz = biz."))))
