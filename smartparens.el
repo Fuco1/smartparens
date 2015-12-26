2289f9eef9251219f2b5889e288cbe9c17cd5d7e
Sam Halliday <sam.halliday@gmail.com>
2015-12-26 00:18:20 +0000

    Interactive behaviour for sp-{next,previous}-sexp
    The current behaviour for sp-{next,previous} is unintiutive in
    interactive mode when the point is not on the :end or :beg respectively.
    Users may expect the following behaviour:
    
       (f|oo bar) -> (foo |bar) ;; sp-next-sexp
    
       (foo b|ar baz) -> (foo| bar baz) ;; sp-previous-sexp
    
    This commit introduces a new defcustom, `sp-interactive-dwim' (nil by
    default) to achieve the desired behaviour. It is only activated if
    sp-{next,previous}-sexp is called interactively.
    
    Note: the (if ...) statements of both implementations have been reversed
    in order to avoid introducing a (progn ...) for the true case.

--- smartparens.el
+++ smartparens.el
@@ -4948,6 +4948,11 @@ Examples: (prefix arg in comment)
 
 (put 'sp-backward-sexp 'CUA 'move)
 
+(defcustom sp-interactive-dwim nil
+  "If non-nil and called `interactive', `sp-next-sexp' and
+`sp-previous-sexp' will automatically move the point to the
+end/beg of the current if point is not already on a beg or end.")
+
 (defun sp-next-sexp (&optional arg)
   "Move forward to the beginning of next balanced expression.
 
@@ -4956,6 +4961,10 @@ at current level, jump one level up (effectively doing
 `sp-backward-up-sexp').  Negative arg -N means move to the
 beginning of N-th previous balanced expression.
 
+If `sp-interactive-dwim' is non-nil, and this is called
+interactively, the point will be moved to the :beg before
+proceeding.
+
 With `sp-navigate-consider-symbols' symbols and strings are also
 considered balanced expressions.
 
@@ -4963,20 +4972,30 @@ Examples:
 
   ((foo) |bar (baz quux)) -> ((foo) bar |(baz quux))
 
-  ((foo) bar |(baz quux)) -> |((foo) bar (baz quux))"
+  ((foo) bar |(baz quux)) -> |((foo) bar (baz quux))
+
+and with non-nil `sp-interactive-dwim'
+
+  (f|oo bar) -> (foo |bar)
+
+  ((fo|o) (bar)) -> (|(foo) (bar))"
   (interactive "^p")
   (setq arg (or arg 1))
-  (if (> arg 0)
-      (if (= arg 1)
-          (-when-let (ok (sp-get-thing))
-            (if (= (point) (sp-get ok :beg))
-                (progn (sp-forward-sexp 2)
-                       (sp-backward-sexp))
-              (goto-char (sp-get ok :beg))
-              ok))
-        (sp-forward-sexp arg)
-        (sp-backward-sexp))
-    (sp-backward-sexp (- arg))))
+  (if (<= arg 0)
+      (sp-backward-sexp (- arg))
+    (when (and sp-interactive-dwim
+               (called-interactively-p 'any)
+               (not (= (point) (sp-get (sp-get-thing) :beg))))
+      (goto-char (sp-get (sp-get-thing) :beg)))
+    (if (= arg 1)
+        (-when-let (ok (sp-get-thing))
+          (if (= (point) (sp-get ok :beg))
+              (progn (sp-forward-sexp 2)
+                     (sp-backward-sexp))
+            (goto-char (sp-get ok :beg))
+            ok))
+      (sp-forward-sexp arg)
+      (sp-backward-sexp))))
 
 (put 'sp-next-sexp 'CUA 'move)
 
@@ -4991,24 +5010,38 @@ N-th following balanced expression.
 With `sp-navigate-consider-symbols' symbols and strings are also
 considered balanced expressions.
 
+If `sp-interactive-dwim' is non-nil, and this is called
+interactively, the point will be moved to the :end before
+proceeding.
+
 Examples:
 
   ((foo) bar| (baz quux)) -> ((foo)| bar (baz quux))
 
-  ((foo)| bar (baz quux)) -> ((foo) bar (baz quux))|"
+  ((foo)| bar (baz quux)) -> ((foo) bar (baz quux))|
+
+and if `sp-interactive-dwim' is non-nil
+
+  (foo b|ar baz) -> (foo| bar baz)
+
+  (f|oo bar baz) -> (foo bar baz)|"
   (interactive "^p")
   (setq arg (or arg 1))
-  (if (> arg 0)
-      (if (= arg 1)
-          (-when-let (ok (sp-get-thing t))
-            (if (= (point) (sp-get ok :end))
-                (progn (sp-backward-sexp 2)
-                       (sp-forward-sexp))
-              (goto-char (sp-get ok :end))
-              ok))
-        (sp-backward-sexp arg)
-        (sp-forward-sexp))
-    (sp-forward-sexp (- arg))))
+  (if (<= arg 0)
+      (sp-forward-sexp (- arg))
+    (when (and sp-interactive-dwim
+               (called-interactively-p 'any)
+               (not (= (point) (sp-get (sp-get-thing) :end))))
+      (goto-char (sp-get (sp-get-thing) :end)))
+    (if (= arg 1)
+        (-when-let (ok (sp-get-thing t))
+          (if (= (point) (sp-get ok :end))
+              (progn (sp-backward-sexp 2)
+                     (sp-forward-sexp))
+            (goto-char (sp-get ok :end))
+            ok))
+      (sp-backward-sexp arg)
+      (sp-forward-sexp))))
 
 (put 'sp-previous-sexp 'CUA 'move)
 
