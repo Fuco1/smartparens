00a25700f9455531ec8a0c9dcf13badfc284a5e7
Matus Goljer <matus.goljer@gmail.com>
2016-05-10 22:31:43 +0200

    Fix tests for 24.3

--- test/smartparens-delayed-hook-test.el
+++ test/smartparens-delayed-hook-test.el
@@ -1,70 +1,73 @@
-(ert-deftest sp-test-delayed-hook-should-trigger-on-RET ()
-  (cl-flet ((sp-test-insert-space
-             (&rest _)
-             (insert " ")))
-    (let ((sp-pairs '((emacs-lisp-mode
-                       (:open "(" :close ")" :actions (insert wrap autoskip navigate)
-                        :post-handlers ((sp-test-insert-space "RET")))))))
-      (sp-test-with-temp-elisp-buffer ""
-        (execute-kbd-macro "(")
-        (execute-kbd-macro (kbd "RET"))
-        (insert "|")
-        (should (equal (buffer-string) "(
+;; TODO: no idea why cl-letf doesn't work on 24.3.  The error is:
+;; (void-function sp-test-insert-space)
+(when (version<= "24.4" emacs-version)
+  (ert-deftest sp-test-delayed-hook-should-trigger-on-RET ()
+    (cl-letf (((symbol-function 'sp-test-insert-space)
+               (lambda (&rest _)
+                 (insert " "))))
+      (let ((sp-pairs '((emacs-lisp-mode
+                         (:open "(" :close ")" :actions (insert wrap autoskip navigate)
+                          :post-handlers ((sp-test-insert-space "RET")))))))
+        (sp-test-with-temp-elisp-buffer ""
+          (execute-kbd-macro "(")
+          (execute-kbd-macro (kbd "RET"))
+          (insert "|")
+          (should (equal (buffer-string) "(
  |)"))))))
 
-(ert-deftest sp-test-delayed-hook-should-not-trigger-on-a ()
-  (cl-flet ((sp-test-insert-space
-             (&rest _)
-             (insert " ")))
-    (let ((sp-pairs '((emacs-lisp-mode
-                       (:open "(" :close ")" :actions (insert wrap autoskip navigate)
-                        :post-handlers ((sp-test-insert-space "RET")))))))
-      (sp-test-with-temp-elisp-buffer ""
-        (execute-kbd-macro "(")
-        (execute-kbd-macro (kbd "a"))
-        (insert "|")
-        (should (equal (buffer-string) "(a|)"))))))
+  (ert-deftest sp-test-delayed-hook-should-not-trigger-on-a ()
+    (cl-letf (((symbol-function 'sp-test-insert-space)
+               (lambda (&rest _)
+                 (insert " "))))
+      (let ((sp-pairs '((emacs-lisp-mode
+                         (:open "(" :close ")" :actions (insert wrap autoskip navigate)
+                          :post-handlers ((sp-test-insert-space "RET")))))))
+        (sp-test-with-temp-elisp-buffer ""
+          (execute-kbd-macro "(")
+          (execute-kbd-macro (kbd "a"))
+          (insert "|")
+          (should (equal (buffer-string) "(a|)"))))))
 
-(ert-deftest sp-test-delayed-hook-should-trigger-on-my/newline ()
-  (cl-letf (((symbol-function 'my/test-newline)
-             (lambda (&rest _)
-               (interactive)
-               (insert "\n")))
-            ((symbol-function 'sp-test-insert-space)
-             (lambda (&rest _)
-               (insert " "))))
-    (let ((sp-pairs '((emacs-lisp-mode
-                       (:open "(" :close ")" :actions (insert wrap autoskip navigate)
-                        :post-handlers ((sp-test-insert-space my/test-newline)))))))
-      (sp-test-with-temp-elisp-buffer ""
-        (execute-kbd-macro "(")
-        (setq this-command 'my/test-newline)
-        (call-interactively 'my/test-newline)
-        (run-hooks 'post-command-hook)
-        (insert "|")
-        (should (equal (buffer-string) "(
+  (ert-deftest sp-test-delayed-hook-should-trigger-on-my/newline ()
+    (cl-letf (((symbol-function 'my/test-newline)
+               (lambda (&rest _)
+                 (interactive)
+                 (insert "\n")))
+              ((symbol-function 'sp-test-insert-space)
+               (lambda (&rest _)
+                 (insert " "))))
+      (let ((sp-pairs '((emacs-lisp-mode
+                         (:open "(" :close ")" :actions (insert wrap autoskip navigate)
+                          :post-handlers ((sp-test-insert-space my/test-newline)))))))
+        (sp-test-with-temp-elisp-buffer ""
+          (execute-kbd-macro "(")
+          (setq this-command 'my/test-newline)
+          (call-interactively 'my/test-newline)
+          (run-hooks 'post-command-hook)
+          (insert "|")
+          (should (equal (buffer-string) "(
  |)"))))))
 
-(ert-deftest sp-test-delayed-hook-should-not-trigger-on-newline ()
-  (cl-letf (((symbol-function 'my/test-newline)
-             (lambda (&rest _)
-               (interactive)
-               (insert "\n")))
-            ((symbol-function 'newline)
-             (lambda (&rest _)
-               (interactive)
-               (insert "\n")))
-            ((symbol-function 'sp-test-insert-space)
-             (lambda (&rest _)
-               (insert " "))))
-    (let ((sp-pairs '((emacs-lisp-mode
-                       (:open "(" :close ")" :actions (insert wrap autoskip navigate)
-                        :post-handlers ((sp-test-insert-space my/test-newline)))))))
-      (sp-test-with-temp-elisp-buffer ""
-        (execute-kbd-macro "(")
-        (setq this-command 'newline)
-        (call-interactively 'newline)
-        (run-hooks 'post-command-hook)
-        (insert "|")
-        (should (equal (buffer-string) "(
-|)"))))))
+  (ert-deftest sp-test-delayed-hook-should-not-trigger-on-newline ()
+    (cl-letf (((symbol-function 'my/test-newline)
+               (lambda (&rest _)
+                 (interactive)
+                 (insert "\n")))
+              ((symbol-function 'newline)
+               (lambda (&rest _)
+                 (interactive)
+                 (insert "\n")))
+              ((symbol-function 'sp-test-insert-space)
+               (lambda (&rest _)
+                 (insert " "))))
+      (let ((sp-pairs '((emacs-lisp-mode
+                         (:open "(" :close ")" :actions (insert wrap autoskip navigate)
+                          :post-handlers ((sp-test-insert-space my/test-newline)))))))
+        (sp-test-with-temp-elisp-buffer ""
+          (execute-kbd-macro "(")
+          (setq this-command 'newline)
+          (call-interactively 'newline)
+          (run-hooks 'post-command-hook)
+          (insert "|")
+          (should (equal (buffer-string) "(
+|)")))))))
