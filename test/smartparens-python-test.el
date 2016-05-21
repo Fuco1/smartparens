5ca4fb62cb9a6cbe22166a0f90cd417f01c6f282
Matus Goljer <matus.goljer@gmail.com>
2016-05-21 13:01:42 +0200

    Remove obsolete test

--- test/smartparens-python-test.el
+++ test/smartparens-python-test.el
@@ -1,15 +1,6 @@
 (require 'ert)
 (require 'smartparens)
 
-(ert-deftest sp-test-sp-autoescape-string-quote-if-empty ()
-  (sp-test-with-temp-buffer "def foo():\n    |"
-      (python-mode)
-    (let ((sp-autoescape-string-quote-if-empty '(python-mode))
-          (sp-autoskip-closing-pair 'always)
-          (sp-undo-pairs-separately nil))
-      (execute-kbd-macro "\"\"\""))
-    (should (equal (buffer-string) "def foo():\n    \"\"\"\"\"\""))))
-
 (ert-deftest sp-test-dont-reindent-python ()
   (sp-test-with-temp-buffer "if foo:
     bar()
