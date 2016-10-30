9d47e4261051766d40ac700ca69179062dbbd3fd
Matus Goljer <matus.goljer@gmail.com>
2016-10-30 11:15:15 +0100

    Update dash dependency

--- smartparens-pkg.el
+++ smartparens-pkg.el
@@ -1,3 +1,3 @@
-(define-package "smartparens" "1.7.1" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
-  '((dash "2.12.1")
+(define-package "smartparens" "1.8.0" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+  '((dash "2.13.0")
     (cl-lib "0.3")))
