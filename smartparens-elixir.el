56c5dc09a50e432c79d0a4d2eff20df1675ca584
Matus Goljer <matus.goljer@gmail.com>
2017-01-15 16:48:44 +0100

    Add basic elixir-lang support

---
+++ smartparens-elixir.el
@@ -0,0 +1,108 @@
+;;; smartparens-elixir.el --- Configuration for Elixir.
+
+;; Copyright (C) 2017 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created: 15th January 2017
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'smartparens)
+
+(defun sp-elixir-def-p (id)
+  "Return non-nil if the \"do\" keyword is part of definition.
+
+Definitions are the constructions of the form defmodule-do-end,
+def-do-end and similar pairs."
+  (save-excursion
+    (when (equal "do" id)
+      (back-to-indentation)
+      (looking-at (regexp-opt '(
+                                "defmodule"
+                                "def"
+                                "if"
+                                "unless"
+                                "case"
+                                "receive"
+                                ))))))
+
+(defun sp-elixir-skip-def-p (ms mb me)
+  (sp-elixir-def-p ms))
+
+(defun sp-elixir-do-block-post-handler (id action context)
+  "Insert \"do\" keyword and indent the new block."
+  (when (eq action 'insert)
+    (let ((m (make-marker)))
+      (save-excursion
+        (forward-word) ;; over the "end"
+        (move-marker m (point)))
+      (save-excursion (newline))
+      (save-excursion (insert " do"))
+      (indent-region (line-beginning-position) m)
+      (move-marker m nil nil))))
+
+(defun sp-elixir-empty-do-block-post-handler (id action context)
+  "Insert empty \"do\" keyword and indent the new block.
+
+This is used for receive-do-end expression."
+  (when (eq action 'insert)
+    (let ((m (make-marker)))
+      (save-excursion
+        (forward-word) ;; over the "end"
+        (move-marker m (point)))
+      (save-excursion
+        (forward-line -1)
+        (end-of-line)
+        (insert " do"))
+      (save-excursion (newline))
+      (indent-region (line-beginning-position) m)
+      (indent-according-to-mode)
+      (move-marker m nil nil))))
+
+(sp-with-modes 'elixir-mode
+  (sp-local-pair "do" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :skip-match 'sp-elixir-skip-def-p)
+  (sp-local-pair "def" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-do-block-post-handler))
+  (sp-local-pair "defmodule" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-do-block-post-handler))
+  (sp-local-pair "fn" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '("| "))
+  (sp-local-pair "if" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-do-block-post-handler))
+  (sp-local-pair "unless" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-do-block-post-handler))
+  (sp-local-pair "case" "end"
+                 :when '(("SPC" "RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-do-block-post-handler))
+  (sp-local-pair "receive" "end"
+                 :when '(("RET" "<evil-ret>"))
+                 :post-handlers '(sp-elixir-empty-do-block-post-handler))
+  )
+
+(provide 'smartparens-elixir)
+;;; smartparens-elixir.el ends here
---
+++ test/smartparens-elixir-test.el
@@ -0,0 +1,117 @@
+(require 'elixir-mode)
+(require 'smartparens-elixir)
+
+(defun sp-test-elixir-parse (initial sexp &optional back)
+  (declare (indent 1))
+  (sp-test-with-temp-buffer initial
+      (elixir-mode)
+    (should (equal (sp-get-thing back) sexp))))
+
+(defun sp-test-insertion-elixir (initial keys result)
+  (sp-test-with-temp-buffer initial
+      (elixir-mode)
+    (execute-kbd-macro keys)
+    (sp-buffer-equals result)))
+
+(ert-deftest sp-test-elixir-parse-defmodule-empty-forward ()
+  "Parse defmodule correctly"
+  (sp-test-elixir-parse "|defmodule HelloWorld do
+end"
+    '(:beg 1 :end 28 :op "defmodule" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-defmodule-empty-backward ()
+  "Parse defmodule correctly"
+  (sp-test-elixir-parse "defmodule HelloWorld do
+end|"
+    '(:beg 1 :end 28 :op "defmodule" :cl "end" :prefix "" :suffix "") :back))
+
+(ert-deftest sp-test-elixir-parse-defmodule-with-method-forward ()
+  "Parse def correctly"
+  (sp-test-elixir-parse "|defmodule HelloWorld do
+  def hello od
+    IO.puts \"Hello world\"
+  end
+end"
+    '(:beg 1 :end 75 :op "defmodule" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-defmodule-with-method-backward ()
+  "Parse def correctly"
+  (sp-test-elixir-parse "defmodule HelloWorld do
+  def hello od
+    IO.puts \"Hello world\"
+  end
+end|"
+    '(:beg 1 :end 75 :op "defmodule" :cl "end" :prefix "" :suffix "") :back))
+
+(ert-deftest sp-test-elixir-parse-defmodule-from-inside-forward ()
+  "Parse def correctly"
+  (sp-test-elixir-parse "defmodule HelloWorld do
+  def hello od
+    IO.puts \"Hello world\"
+  end|
+end"
+    '(:beg 1 :end 75 :op "defmodule" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-def-forward ()
+  "Parse def correctly"
+  (sp-test-elixir-parse "defmodule HelloWorld do
+  |def hello od
+    IO.puts \"Hello world\"
+  end
+end"
+    '(:beg 27 :end 71 :op "def" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-def-backward ()
+  "Parse def correctly"
+  (sp-test-elixir-parse "defmodule HelloWorld do
+  def hello od
+    IO.puts \"Hello world\"
+  end|
+end"
+    '(:beg 27 :end 71 :op "def" :cl "end" :prefix "" :suffix "") :back))
+
+(ert-deftest sp-test-elixir-parse-fn-multiline ()
+  "Parse fn-end pair"
+  (sp-test-elixir-parse "def la do
+  task = Task.async |fn ->
+    IO.puts \"remote print\"
+    :timer.sleep(1000)
+  end
+  IO.puts \"#{inspect Task.await task}\"
+end"
+    '(:beg 31 :end 92 :op "fn" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-if-forward ()
+  "Parse if-end pair"
+  (sp-test-elixir-parse "def world do
+  |if true do
+    IO.puts \"hello\"
+  end
+
+  unless false do
+    IO.puts \"world\"
+  end
+end"
+    '(:beg 16 :end 52 :op "if" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-parse-unless-forward ()
+  "Parse if-end pair"
+  (sp-test-elixir-parse "def world do
+  if true do
+    IO.puts \"hello\"
+  end
+
+  |unless false do
+    IO.puts \"world\"
+  end
+end"
+    '(:beg 56 :end 97 :op "unless" :cl "end" :prefix "" :suffix "")))
+
+(ert-deftest sp-test-elixir-receive-block-insertion ()
+  (sp-test-insertion-elixir "|" "receive" "receive do
+  |
+end"))
+
+(ert-deftest sp-test-elixir-case-block-insertion ()
+  (sp-test-insertion-elixir "|" "case " "case | do
+end"))
