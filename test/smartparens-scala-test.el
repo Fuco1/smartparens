10657ccd634804de2b43a1fa65d7e2c010d1c706
Matus Goljer <matus.goljer@gmail.com>
2017-07-01 23:17:30 +0200

    Fix kbd usage in tests

--- test/smartparens-scala-test.el
+++ test/smartparens-scala-test.el
@@ -3,56 +3,43 @@
 
 (ert-deftest sp-test-scala-bracket ()
   "Close a normal bracket"
-  (sp-test-with-temp-buffer
-   "def foo|"
-   (scala-mode)
-   (execute-kbd-macro "(bar: String")
-   (should (equal (buffer-string) "def foo(bar: String)"))))
+  (sp-test-with-temp-buffer "def foo|"
+      (scala-mode)
+    (execute-kbd-macro "(bar: String")
+    (should (equal (buffer-string) "def foo(bar: String)"))))
 
 (ert-deftest sp-test-scala-bracket-space ()
   "Close a normal bracket with padding on SPACE."
-  (sp-test-with-temp-buffer
-   "foo.map|"
-   (scala-mode)
-   (execute-kbd-macro "(")
-   (execute-kbd-macro (kbd "SPC"))
-   (execute-kbd-macro "_.toString")
-   (should (equal (buffer-string) "foo.map( _.toString )"))))
+  (sp-test-with-temp-buffer "foo.map|"
+      (scala-mode)
+    (execute-kbd-macro "( _.toString")
+    (should (equal (buffer-string) "foo.map( _.toString )"))))
 
 (ert-deftest sp-test-scala-curly ()
   "Close a curly bracket"
-  (sp-test-with-temp-buffer
-   "foo.map|"
-   (scala-mode)
-   (execute-kbd-macro "{f => f.toString")
-   (should (equal (buffer-string) "foo.map{f => f.toString}"))))
+  (sp-test-with-temp-buffer "foo.map|"
+      (scala-mode)
+    (execute-kbd-macro "{f => f.toString")
+    (should (equal (buffer-string) "foo.map{f => f.toString}"))))
 
 (ert-deftest sp-test-scala-curly-space ()
   "Close a curly bracket with padding on SPACE"
-  (sp-test-with-temp-buffer
-   ;; it might be nice in a future update to not need the prefix space
-   "foo.map |"
-   (scala-mode)
-   (execute-kbd-macro "{")
-   (execute-kbd-macro (kbd "SPC"))
-   (execute-kbd-macro "f => f.toString")
-   (should (equal (buffer-string) "foo.map { f => f.toString }"))))
+  (sp-test-with-temp-buffer "foo.map |"
+      ;; it might be nice in a future update to not need the prefix space
+      (scala-mode)
+    (execute-kbd-macro "{ f => f.toString")
+    (should (equal (buffer-string) "foo.map { f => f.toString }"))))
 
 (ert-deftest sp-test-scala-curly-newline ()
   "Close a curly bracket with an indented block on newline"
-  (sp-test-with-temp-buffer
-   "foo.map |"
-   (scala-mode)
-   (execute-kbd-macro "{")
-   (execute-kbd-macro (kbd "RET"))
-   (execute-kbd-macro "f => f.toString")
-   (should (equal (buffer-string) "foo.map {\n  f => f.toString\n}"))))
+  (sp-test-with-temp-buffer "foo.map |"
+      (scala-mode)
+    (execute-kbd-macro (kbd "{ RET f SPC => SPC f.toString"))
+    (sp-buffer-equals "foo.map {\n  f => f.toString|\n}")))
 
 (ert-deftest sp-test-scala-curly-wrap ()
   "Wrap a region in an indented block"
-  (sp-test-with-temp-buffer
-   "|foo|"
-   (scala-mode)
-   (execute-kbd-macro "{")
-   (should (equal (buffer-string) "{\n  foo\n}"))))
-
+  (sp-test-with-temp-buffer "|fooM"
+      (scala-mode)
+    (execute-kbd-macro "{")
+    (should (equal (buffer-string) "{\n  foo\n}"))))
