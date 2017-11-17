4873352b5d0a1c5142658122de1b6950b8fe7e4d
Matus Goljer <matus.goljer@gmail.com>
2017-11-17 15:33:41 +0100

    Release 1.11.0

--- smartparens-pkg.el
+++ smartparens-pkg.el
@@ -1,3 +1,3 @@
-(define-package "smartparens" "1.10.1" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
+(define-package "smartparens" "1.11.0" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
   '((dash "2.13.0")
     (cl-lib "0.3")))
