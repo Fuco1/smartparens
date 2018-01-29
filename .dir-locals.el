e487f20231a1cb304c27f7fd0ba3a1c81942ae0a
Alex Branham <branham@utexas.edu>
2018-01-29 10:58:51 -0600

    dir-locals: add bug-reference-url-format

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((bug-reference-url-format . "https://github.com/Fuco1/smartparens/issues/%s"))))
