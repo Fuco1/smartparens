8efc55c8708bcff93c375218bc8c0e2582f14b09
Matus Goljer <matus.goljer@gmail.com>
2018-03-04 17:56:19 +0100

    [Fix #782] Add the issue report setup as test case
    Issue was fixed in 826bdcd along with #786.

--- test/smartparens-elisp-test.el
+++ test/smartparens-elisp-test.el
@@ -13,3 +13,15 @@
 (ert-deftest sp-test-elisp-invalid-hyperlink/point-after-before-punctuation ()
   (sp-test-with-temp-elisp-buffer "; foo'|."
     (should-not (sp-lisp-invalid-hyperlink-p nil 'navigate nil))))
+
+;; #782
+(ert-deftest sp-test-elisp-invalid-hyperlink/strict-mode-should-delete-over-contraction-after-hyperlink ()
+  "In case there was a hyperlink followed by punctuation and a
+contraction after that the parser figured that the hyperlink
+actually ends at the contraction because the quote before the
+punctuation was considered invalid."
+  (sp-test-with-temp-elisp-buffer ";; `a-symbol-name'? I'm|"
+    (smartparens-strict-mode 1)
+    (sp-backward-delete-char)
+    (sp-backward-delete-char)
+    (sp-buffer-equals ";; `a-symbol-name'? I|")))
