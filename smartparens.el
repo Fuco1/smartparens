5014344246c99890e2825a6d2160275d53627e64
Matus Goljer <matus.goljer@gmail.com>
2018-08-08 19:58:12 +0200

    [Fix #914] Add haml-mode to sp-no-reindent-after-kill-modes

--- smartparens.el
+++ smartparens.el
@@ -605,6 +605,7 @@ Symbol is defined as a chunk of text recognized by
                                              coffee-mode
                                              asm-mode
                                              makefile-gmake-mode
+                                             haml-mode
                                              )
   "List of modes that should not reindent after kill."
   :type '(repeat symbol)
