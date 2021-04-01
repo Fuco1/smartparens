88fdf57cce6c4f62aacee5b87009823f8114fe17
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2021-04-01 17:24:54 +0300

    Add minimal Emacs version

--- smartparens-pkg.el
+++ smartparens-pkg.el
@@ -1,3 +1,4 @@
 (define-package "smartparens" "1.11.0" "Automatic insertion, wrapping and paredit-like navigation with user defined pairs."
-  '((dash "2.13.0")
+  '((emacs "24.4")
+    (dash "2.13.0")
     (cl-lib "0.3")))
