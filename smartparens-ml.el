e0f5feb386cfd58e3e96d45e1b54b35e88c45304
Sangwoo Joh <work.sangwoo.joh@gmail.com>
2021-12-22 12:00:45 +0900

    :sparkles: add array pair

--- smartparens-ml.el
+++ smartparens-ml.el
@@ -61,6 +61,7 @@
   ;; Disable ' because it is used in value names and types
   (sp-local-pair "'" nil :actions nil)
   (sp-local-pair "{|" "|}" )      ;; multi-line string
+  (sp-local-pair "[|" "|]" )      ;; array
   (sp-local-pair "sig" "end" )    ;; signature
   (sp-local-pair "module" "end" ) ;; module
   (sp-local-pair "(*" "*)" ))     ;; comment
