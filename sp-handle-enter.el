a077d9b4af834bbda7c552123550a5f37b493fd8
semenInRussia <hrams205@gmail.com>
2022-01-16 19:30:00 +0300

    sp-handle-enter.el: feature add able to customization

--- sp-handle-enter.el
+++ sp-handle-enter.el
@@ -64,44 +64,87 @@
   :group 'smartparens)
 
 
-(defcustom sp-handle-enter-in-quotes t
-  "When non-nil, if user type newline, handle it."
+(defcustom sp-handle-enter-in-quotes-p nil
+  "When non-nil, if user type newline in quotes, handle it."
   :group 'sp-handle-enter
   :type 'bool)
 
 
-(defcustom sp-handle-enter-in-parens '("{" "(" "[")
+(defcustom sp-handle-enter-in-comment-p nil
+  "When non-nil, if user type newline in comment, handle it."
+  :group 'sp-handle-enter
+  :type 'bool)
+
+
+(defcustom sp-handle-enter-parens '("{" "(" "[")
   "List of opening parens in which will handle Enter."
   :group 'sp-handle-enter
   :type '(repeat string))
 
 
-(--each sp-handle-enter-in-parens
-  (sp-pair it nil :post-handlers '(("||\n[i]" "RET"))))
+(--each sp-handle-enter-parens
+    (sp-pair it nil
+             :post-handlers '((sp-handle-enter--after-newline "RET"))))
+
+
+(defun sp-handle-enter--after-newline (id action context)
+    "Insert additional newline and go to special position.
+With ID, ACTION, CONTEXT.  Example:
+
+   dict = {
+   |}
+
+Call function:
+
+   dict = {
+       |
+   }"
+    (when (sp-handle-enter--p id action context)
+        (forward-line -1)
+        (end-of-line)
+        (newline-and-indent)))
+
+
+(defun sp-handle-enter--p (id action context)
+    "Return non-nil when need to handle enter.
+ID, ACTION, CONTEXT"
+    (setq context (save-excursion (forward-char -1)
+                                  (sp--get-context)))
+
+    (and (if sp-handle-enter-in-comment-p
+             t
+             (not (sp-in-comment-p id action context)))
+         (if sp-handle-enter-in-quotes-p
+             t
+             (not (sp-in-string-quotes-p id action context)))))
 
 
 (sp-with-modes '(haskell-mode haskell-interactive-mode)
-  (sp-local-pair "{" nil :post-handlers '((sp-handle-enter--open-{ "RET"))))
+    (sp-local-pair "{" nil
+                   :post-handlers '((sp-handle-enter--open-{ "RET"))))
 
 
-(defun sp-handle-enter--haskell-open-{ (&rest _ignored)
-    "Haskell {} block for `smartparens'.
+(defun sp-handle-enter--haskell-open-{ (id action context)
+    "Haskell {} block for `smartparens'.  With ID, ACTION, CONTEXT.
 For example:
+
     newtype Book = {
 |}
+
 to:
+
     newtype Book = {|
                    }"
-  (interactive)
-  (let* ((prev-line-len (save-excursion
-                            (forward-line -1)
-                            (length (s-chop-suffix "\n"
-                                                   (thing-at-point 'line t))))))
-      (indent-to prev-line-len)
-      (forward-line -1)
-      (end-of-line))
-  )
+    (when (sp-handle-enter--p id action context)
+        (let* ((prev-line-len (save-excursion
+                                  (forward-line -1)
+                                  (length (s-chop-suffix "\n"
+                                                         (thing-at-point 'line
+                                                                         t))))))
+            (indent-to prev-line-len)
+            (forward-line -1)
+            (end-of-line))))
+
 
 (provide 'sp-handle-enter)
-
 ;;; sp-handle-enter.el ends here
